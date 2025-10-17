f07ec8401265b61fdb6aa8e3e77a74be6bd25078
jaffer123 <jaffersathik225@gmail.com>
2025-10-17 15:50:14 +0530

    changes

--- nodejs/utility/mainUtility.js
+++ nodejs/utility/mainUtility.js
@@ -1,34 +1,29 @@
-var jwtToken =require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-parseCookie = function(cookie) {
-    console.log('parser maintain');
-      var cookies = cookie.split(',')
-      var cookies_obj  = {};
-      for(var i = 0 ; i < cookies.length ; i++) {
-        var key_val_pair = cookies[i].split("=")
-        cookies_obj[key_val_pair[0]]=key_val_pair[1];
-      }
-      return cookies_obj; 
-    }
-decodeToken = async function(token,req,res) {
-        jwtToken.verify(token, 'ownbudget-authkey', function(err, decoded) {
-          if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-          return decoded;
-        });
-    }
+const parseCookie = (cookie = '') => {
+  return cookie.split(';').reduce((acc, part) => {
+    const [key, value] = part.trim().split('=');
+    if (key) acc[key] = value;
+    return acc;
+  }, {});
+};
 
-  parseAuth = function(auth){
-    console.log(parseAuth);
-    let auth_token='';
-    let auth_obj = auth.split(" ");
-    auth_obj.map(dt=>{
-      auth_token=dt;
-    });
-  return auth_token;
-    }
+const decodeToken = (token, res) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET || 'ownbudget-authkey');
+  } catch (err) {
+    return res.status(401).json({ auth: false, message: 'Invalid token',err });
+  }
+};
 
-module.exports={
-    parseCookie,
-    decodeToken,
-    parseAuth,
-}
+const parseAuth = (authHeader = '') => {
+  return authHeader.startsWith('Bearer ')
+    ? authHeader.split(' ')[1]
+    : '';
+};
+
+module.exports = {
+  parseCookie,
+  decodeToken,
+  parseAuth,
+};
